ac65c9f10ca88d2ef12d3c0dfd385a290981c8c1
Bryan Thomas <bryan@Bryans-MacBook-Pro.local>
2020-11-29 16:51:48 -0700

    update return properties

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var AWS = require("aws-sdk");
 var ddb = new AWS.DynamoDB.DocumentClient({region: "us-west-2"});
 
 exports.handler = async (event, context, callback) => {
- await readProperties().then(data => {
+    
+ return await readProperties().then(data => {
      data.Items.forEach(function(item){
          console.log(item.address)
      })
+     return data.Items
  }).catch((err) => {
      console.error(err);
  })
